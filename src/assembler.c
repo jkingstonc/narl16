875fbca35f67a9202d3fd907c323077f9c74ed98
James Clarke <jkc200013@gmail.com>
2019-05-20 22:54:42 +0100

    Updated readme. Added some boilerplate to src files.

--- src/assembler.c
+++ src/assembler.c
@@ -3,3 +3,16 @@ James Clarke 20/05/19
 assembler.c
 This file takes a .narl file as an input and assembles it to a binary
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Strings corresponding to indexes in the registers
+const char *reg_str[] = {"pc", "sp", "ia", "cr", "r1","r2","r3","r4","r5","r6","r7","r8","r9","r10","r11","r12"};
+
+#define MAX_REG 12
+
+int main()
+{
+    printf("%d\n", GET_REG_INDEX("sp"));
+}
--- src/narl16.c
+++ src/narl16.c
@@ -3,3 +3,27 @@ James Clarke 20/05/19
 assembler.c
 This file is the first standard emulator implementation of the narl16 ISA.
 */
+
+#include <stdio.h>
+
+// Macros defining the base address values for areas of the memory
+#define TEXT_ADDR 0x2000
+#define STATIC_ADDR 0x6000
+#define HEAP_ADDR 0x7000
+#define STACK_ADDR 0xE000
+#define VIDEO_ADDR 0xF000
+
+// Cpu represents an instance of the narl16 CPU
+typedef struct Cpu{
+    // Contain register values
+    int registers[12];
+}Cpu;
+
+#define GET_PC(cpu) ((cpu)->registers[0])
+
+int main()
+{
+    Cpu cpu ={{0}};
+    printf("%d\n",GET_PC(&cpu));
+    return 0;
+}
