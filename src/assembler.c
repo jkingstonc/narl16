28170b5e5c5590c7105a349305230e8fc45563e7
James Clarke <jkc200013@gmail.com>
2019-05-21 15:25:48 +0100

    Fixed file loading bug and added 'make_bytecode' template

--- src/assembler.c
+++ src/assembler.c
@@ -18,6 +18,8 @@ This file takes a .narl file as an input and assembles it to a binary
 const char *reg_str[] = {"pc", "sp", "ia", "cr", "r1","r2","r3","r4","r5","r6","r7","r8","r9","r10","r11","r12"};
 // The program string
 char prog[MAX_PROG_LEN][MAX_LINE_LEN];
+// The assembled binaries
+short * prog_assembled;
 
 // Get a register index from a register char... this should be a macro
 int get_reg_index(char *reg){
@@ -34,15 +36,19 @@ int load_prog(char* prog_name)
 {
 	FILE * file = NULL;
 	file = fopen(prog_name,"r");
+
+	if(file==NULL) return -1;
+
 	int line_counter=0;
 	// Loop through each file line and add it to the prog array
-	while(fgets(prog[line_counter],MAX_LINE_LEN,file))
+	while(fgets(prog[line_counter],MAX_LINE_LEN,file)!=NULL)
 	{
 		// Add a null terminator to the line string
 		prog[line_counter][strlen(prog[line_counter])-1]='\0';
 		line_counter++;
 	}
 	int i;
+	// print the program
 	for(i=0;i<line_counter;i++)
 	{
 		printf("%s\n", prog[i]);
@@ -52,6 +58,11 @@ int load_prog(char* prog_name)
 
 int make_bytecode()
 {
+	int line_counter;
+	for(line_counter=0;line_counter<MAX_PROG_LEN;line_counter++)
+	{
+		// First check if we have a macro or instruction
+	}
     return 0;
 }
 
