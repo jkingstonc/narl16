74d692b1ddfb047f3f965787b54f72c3f557f20f
James Clarke <jkc200013@gmail.com>
2019-05-21 17:44:23 +0100

    added method to get opcode value, and added opcode str array.

--- src/assembler.c
+++ src/assembler.c
@@ -13,6 +13,12 @@ This file takes a .narl file as an input and assembles it to a binary
 #define MAX_REG 12
 #define MAX_PROG_LEN 256
 #define MAX_LINE_LEN 128
+#define OP_COUNT 28
+
+const char *opcodes[] = 
+    {"NOP","SET","ADD","SUB","MUL","DIV","AND","OR","XOR","NOT",
+     "MOD", "REM", "SRL", "SLL", "SRA", "SLA", "IEQ", "INE", "IGE", 
+     "IGT", "ILT", "ILE", "IBS", "INB", "JAL", "RTN", "SYS", "INT"};
 
 // Strings corresponding to indexes in the registers
 const char *reg_str[] = {"pc", "sp", "ia", "cr", "r1","r2","r3","r4","r5","r6","r7","r8","r9","r10","r11","r12"};
@@ -70,20 +76,47 @@ int load_prog(char* prog_name)
     return 0;
 }
 
+short get_op_val(char * op)
+{
+    int i;
+    for(i=0;i<OP_COUNT;i++)
+    {
+        if(strcmp(op, opcodes[i])==0)
+        {
+            return (short)i;
+        }
+    }
+    return -1;
+}
+
 int make_bytecode()
 {
 	int line_counter;
 	for(line_counter=0;line_counter<MAX_PROG_LEN;line_counter++)
 	{
         short byte=0;
-		int counter=0;
+        int counter=0;
+        // allocate enough space for 4 words per line
+		char ** line_words = malloc(4*sizeof(char*));
         char * next = strtok(prog[line_counter]," ,");
         while(next != NULL)
         {
-            printf("%s\n",next);
+            // allocate more space for the current next value
+            line_words[counter]=(char*)malloc(sizeof(next));
+            // copy the next value to the array
+            strcpy(line_words[counter],next);
+            // get the next word
             next = strtok(NULL, " ,");
+            counter++;
         }
-        getchar();
+        // check if we have encountered a macro
+        if(strcmp(line_words[0],"#")==0)
+        {
+
+        }else
+        {
+
+        }
 	}
     return 0;
 }
