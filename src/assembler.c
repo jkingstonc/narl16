8bd25480406f72789e2e8fc0f6e73ea36056ebb9
James Clarke <jkc200013@gmail.com>
2019-05-22 11:51:52 +0100

    added writing to file and extra-word immediates

--- src/assembler.c
+++ src/assembler.c
@@ -18,7 +18,7 @@ This file takes a .narl file as an input and assembles it to a binary
 #define MAX_XY_FUNCS 3
 
 typedef struct {
-    short * array;
+    unsigned short * array;
     size_t used;
     size_t size;
 } BytecodeArray;
@@ -32,12 +32,13 @@ void init_bytecode_array(BytecodeArray * a, size_t initial)
 }
 
 // Dynamically insert an element to the array
-void insert_bytecode_array(BytecodeArray * a, short element)
+void insert_bytecode_array(BytecodeArray * a, unsigned short element)
 {
+    printf("Inserting %04x\n",element);
     if(a->used==a->size)
     {
-        a->size*=2;
-        a->array=(short*)realloc(a->array, a->size * sizeof(short));
+        a->size+=1;
+        a->array=(short*)realloc(a->array, a->size * sizeof(unsigned short));
     }
     a->array[a->used++]=element;
 }
@@ -64,6 +65,24 @@ const char *xy_funcs[] = {"PSH","POP","PEEK"};
 char prog[MAX_PROG_LEN][MAX_LINE_LEN];
 // The assembled binaries
 BytecodeArray bytecode_array;
+
+
+// Write the bytecode array to disk
+int write_bytecode(char * name)
+{
+    // int i;
+    // for(i=0;i<(&bytecode_array)->used;i++)
+    // {
+    //     printf("%x\n",(&bytecode_array)->array[i]);
+    // }
+    FILE * write_file;
+    write_file=fopen("narl_binary.bin","wb");
+    fwrite(&bytecode_array, sizeof(short), sizeof(bytecode_array), write_file);
+    fclose(write_file);
+    free_bytecode_array(&bytecode_array);
+    return 0;
+}
+
 
 // Check if an opcode string exists and return it's index
 int check_op_index(char * op)
@@ -197,8 +216,6 @@ int make_bytecode()
 	{
         // Check if the line is empty
         if(prog[line_counter][0] == '\0') continue;
-
-        printf("line: %s\n",prog[line_counter]);
         // Bytecode to be modified
         unsigned short bytecode=0;
         // The word we are in on the raw string
@@ -207,38 +224,54 @@ int make_bytecode()
         char * next = strtok(prog[line_counter]," ,");
 
         // Check if we are lexing macro
-        if(strcmp(next,"#")==0) {printf("Encountered macro...\n");continue;}
+        if(strcmp(next,"#")==0) {continue;}
+
+        // for either x or y, they may need an extra word for an immediate value
+        int sx_flag=0, usx_flag=0, sy_flag=0, usy_flag=0;
+        short sx=0, sy =0;
+        unsigned short usx=0, usy=0;
         // Else lex standard statement
         while(next != NULL)
         {
-            // for either x or y, they may need an extra word for an immediate value
-            int s_flag, us_flag=0;
-            short s=0;
-            unsigned short us=0;
+            int temp_s_flag=0, temp_us_flag=0;
+            short temp_s=0;
+            unsigned short temp_us=0;
+
             // Get the value that should be inserted into the oxy position
-            int val = (counter==0) ? check_op_index(next) : get_xy_val(next,&s,&us,&s_flag,&us_flag);
+            int val = (counter==0) ? check_op_index(next) : get_xy_val(next,&temp_s,&temp_us,&temp_s_flag,&temp_us_flag);
             if(val<0) return -1;
             // Get the bit position in the bytecode that it should be inserted into
             int bit_position = (counter==0) ? 0 : 6+(5*(counter-1));
             // Set the bytecode bits
             bytecode |= (val << bit_position);
-            // Insert the bytecode to the bytecode_array
-            insert_bytecode_array(&bytecode_array,bytecode);
-            // Insert the optional extended word immediate values
-            if(s_flag)insert_bytecode_array(&bytecode_array,s);
-            if(us_flag)insert_bytecode_array(&bytecode_array,us);
-
-            printf("short:  %d\n", s);
-            printf("ushort: %hu\n", us);
+            // Set the optional extended word immediate values
+            if(temp_s_flag)
+            { 
+                if(counter==1) 
+                {
+                    sx=temp_s;
+                    sx_flag=1;
+                }else if(counter==2)
+                {
+                    sy=temp_s;
+                    sy_flag=1;
+                }
+            }
+            if(temp_us_flag){ if(counter==1) {usx=temp_us;usx_flag=1;}else if(counter==2){usy=temp_us;usy_flag=1;}}
             // Get the next word & increase the counter
             next = strtok(NULL, " ,");
             counter++;
         }
-        printf("Bytecode: %x\n",bytecode);
+        // Insert the bytecode to the bytecode array
+        insert_bytecode_array(&bytecode_array,bytecode);
+        // Insert optional multi-word immediates
+        if(sx_flag)insert_bytecode_array(&bytecode_array,sx);
+        else if(usx_flag)insert_bytecode_array(&bytecode_array,usx);
+        if(sy_flag)insert_bytecode_array(&bytecode_array,sy);
+        else if(usy_flag)insert_bytecode_array(&bytecode_array,usy);
 	}
     return 0;
 }
-
 
 // Load the program line by line into an array of lines
 int load_prog(char* prog_name)
@@ -258,18 +291,15 @@ int load_prog(char* prog_name)
 		line_counter++;
 	}
 	int i;
-	// print the program
-	for(i=0;i<line_counter;i++)
-	{
-		printf("%s\n", prog[i]);
-	}
 
     return 0;
 }
+
 
 int main(int argc, char *argv[])
 {
     if(load_prog(argv[1])<0){printf("Failed to load program!\n");return -1;};
     if(make_bytecode()<0) {printf("Failed to assemble!\n");return -1;};
+    if(write_bytecode(argv[1])<0) {printf("Failed to write to disk!\n");return -1;}
     return 0;
 }
